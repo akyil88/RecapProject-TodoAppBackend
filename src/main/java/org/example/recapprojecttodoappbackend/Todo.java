334b6093e6e83505e8352759b43a6aaa4b7ff887
Gökhan Akyil <akyil88@outlook.de>
2024-05-18 22:46:18 +0300

    Post has been repaired

---
+++ src/main/java/org/example/recapprojecttodoappbackend/Todo.java
@@ -0,0 +1,2 @@
+package org.example.recapprojecttodoappbackend;public record Todo() {
+}
---
+++ src/test/java/org/example/recapprojecttodoappbackend/RecapProjectTodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recapprojecttodoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProjectTodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
